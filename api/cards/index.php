a7a798621a7c04b3ba81b60b53f49ad78c2b8b36
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-01-23 19:51:34 +0300

    get cards

---
+++ api/cards/index.php
@@ -0,0 +1,34 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$connect = mysql_connect("localhost","root","") or die(mysql_error());
+mysql_select_db("hon_hunt", $connect) or die(mysql_error()	);
+mysql_query('SET NAMES utf8');
+
+
+// start get
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+  $result = mysql_query("SELECT * FROM Cards") or die(mysql_error());
+  $data = mysql_fetch_assoc($result);
+  $outp = "";
+
+  $emparray = array();
+      do
+     {
+         $emparray[] = $data;
+     } while($data =mysql_fetch_assoc($result));
+  mysql_close();
+
+  echo json_encode($emparray);
+}
+// end get
+
+// start post
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+}
+// end post
+
+?>
